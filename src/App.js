f5e89e84d7ef3557434bf70b6132728be426b242
dongyeon.kim <dongyeon.kim@lasee.io>
2023-04-06 11:06:39 +0900

    카테고리 선택 ui 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import Categories from './components/Categories';
 import NewsList from './components/NewsList';
+import { useCallback } from 'react';
 
 const App = () => {
-    return <NewsList />;
+    const [category, setCategory] = useState('all');
+    const onSelect = useCallback((category) => setCategory(category), []);
+
+    return (
+        <>
+            <Categories category={category} onSelect={onSelect} />
+            <NewsList category={category} />
+        </>
+    );
 };
 
 export default App;
