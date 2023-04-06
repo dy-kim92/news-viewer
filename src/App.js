98909708548b53b6c31f4dbdd064babb472424dc
dongyeon.kim <dongyeon.kim@lasee.io>
2023-04-06 11:33:13 +0900

    리액트 라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import { useState } from 'react';
-import Categories from './components/Categories';
-import NewsList from './components/NewsList';
-import { useCallback } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-    const [category, setCategory] = useState('all');
-    const onSelect = useCallback((category) => setCategory(category), []);
-
     return (
-        <>
-            <Categories category={category} onSelect={onSelect} />
-            <NewsList category={category} />
-        </>
+        <Routes>
+            <Route path="/" element={<NewsPage />} />
+            <Route path="/:category" element={<NewsPage />} />
+        </Routes>
     );
 };
 
